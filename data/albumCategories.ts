706c6354845c20d250a916030e923c4b890dd653
MarianaCarvTeixeira <mariana.silva.teixeira@hotmail.com>
2021-09-13 11:50:01 -0300

    Categories Scroll

---
+++ data/albumCategories.ts
@@ -0,0 +1,295 @@
+
+export default [{
+    id: '1',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '2',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '3',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '4',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '5',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '6',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  },
+  {
+    id: '7',
+    title: 'Favoritos',
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '2',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '3',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '4',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '5',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '6',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      },
+      {
+        id: '7',
+        imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
+        artistsHeadLine: 'Ana Vitoria',
+      }
+    ]
+  
+  }]
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,54 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import AlbumCategory from '../components/AlbumCategory';
-
-const albumCategory = {
-  id: '1',
-  title: 'Favoritos',
-  albums: [
-    {
-      id: '1',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '2',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '3',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '4',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '5',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '6',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    },
-    {
-      id: '7',
-      imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71D5mOi3SdL._AC_SL1000_.jpg',
-      artistsHeadLine: 'Ana Vitoria',
-    }
-  ]
-
-}
+import albumCategories from '../data/albumCategories';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <AlbumCategory title={ albumCategory.title} albums={albumCategory.albums} />
+      <FlatList
+      data={albumCategories}
+      renderItem={({item})=> 
+      <AlbumCategory title={item.title} albums={item.albums}/>}
+      keyExtractor={(item)=> item.id}
+      /> 
     </View>
   );
 }
